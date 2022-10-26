76196e3add57088ed8f37ee43e880cc70762f36d
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2022-10-26 11:52:31 +0700

    initial commit

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
-echo "Hello World";
 
-$nama = "Okta";
-$usia = "21";
+    echo "Hello World!<br> ";
 
-echo "Nama saya $nama, saya berumur $usia";
+    $nama = "Oktaviano";
+    $umur =  21;
+
+    echo "Nama Saya <strong>$nama</strong>, Saya berusia $umur tahun.<br>";
+
+    $namaKakak = "Victoria";
+    $umurKakak =  26;
+
+    echo "Nama Kakak Saya <strong>$namaKakak</strong>, Kakak Saya berusia $umurKakak tahun.<br>";
+
+    $selisihUmur = $umurKakak - $umur;
+
+    echo "Selisih usia Saya dan Kakak saya adalah $selisihUmur tahun"
 
 ?>
