2d2cbbdfcc6ee976c4799c9d151aa25cae299b1d
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2022-10-26 12:48:34 +0700

    initial commit

--- index.php
+++ index.php
@@ -5,15 +5,15 @@
     $nama = "Oktaviano";
     $umur =  21;
 
-    echo "Nama Saya <strong>$nama</strong>, Saya berusia $umur tahun.<br>";
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
 
     $namaKakak = "Victoria";
     $umurKakak =  29;
 
-    echo "Nama Kakak Saya <strong>$namaKakak</strong>, Kakak Saya berusia $umurKakak tahun.<br>";
+    echo "Nama Kakak saya <strong>$namaKakak</strong>, Kakak saya berusia $umurKakak tahun.<br>";
 
     $selisihUmur = $umurKakak - $umur;
 
-    echo "Selisih usia Saya dan Kakak saya adalah $selisihUmur tahun"
+    echo "Selisih usia saya dan Kakak saya adalah $selisihUmur tahun"
 
 ?>
